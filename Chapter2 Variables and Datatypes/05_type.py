2fd3007ab428f67413caf89e98e52a32714c5b89
Sushmita Malakar <sushmalakar10@gmail.com>
2025-12-05 13:11:59 +0545

    type, typecasting and input

---
+++ Chapter2 Variables and Datatypes/05_type.py
@@ -0,0 +1,16 @@
+a = 10
+print(type(a)) 
+
+b = "Hello"
+print(type(b))
+
+a = "3.5"
+print(type(a)) 
+
+b = float(a)
+print(type(b))
+
+
+print(type(str(32)))
+int("32")
+float("32.5")
---
+++ Chapter2 Variables and Datatypes/06_input.py
@@ -0,0 +1,10 @@
+a = input("Enter num 1: ")
+b = input("Enter num 2: ")
+
+print("Sum = ", a+b)
+
+
+a = int(input("Enter num 1: "))
+b = int(input("Enter num 2: "))
+
+print("Sum = ", a+b)
