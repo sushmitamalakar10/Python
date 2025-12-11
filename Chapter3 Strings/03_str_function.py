d58107d190503339f91a80b1c01e13b8cfc9fb83
Sushmita Malakar <sushmalakar10@gmail.com>
2025-12-11 09:31:09 +0545

    String functions

---
+++ Chapter3 Strings/03_str_function.py
@@ -0,0 +1,8 @@
+name = "sushmita"
+
+print(len(name)) # 8
+
+print(name.endswith("ita")) # True
+print(name.startswith("Su")) # True
+print(name.startswith("su")) # False
+print(name.capitalize()) # Sushmita - capitalizes first character
