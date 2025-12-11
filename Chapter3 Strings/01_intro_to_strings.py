c682e33a1aa0f52d89c02d9dde635b5d468e6d82
Sushmita Malakar <sushmalakar10@gmail.com>
2025-12-11 09:13:41 +0545

    Intro to Strings

---
+++ Chapter3 Strings/01_intro_to_strings.py
@@ -0,0 +1,12 @@
+name = "Sushmita"
+print(name)
+print(len(name))  # including spaces
+
+sl = name[0:3] # start from index 0 to all the way till 3 (excluding 3)
+print(sl)
+
+character1 = name[0]
+print(character1)
+
+sl1 = name[-4:-1]
+print(sl1)  # empty string, as -4 is excluded
---
+++ Chapter3 Strings/02_negative_slicing.py
@@ -0,0 +1,11 @@
+name = "Sushmita"
+print(name[0:3])
+
+print(name[-4:-1])
+print(name[4:7])
+
+print(name[:4]) # name[0:4]
+print(name[1:]) # name[1:8]
+print(name[1:8])
+
+print(name[:]) # name[0:8]
