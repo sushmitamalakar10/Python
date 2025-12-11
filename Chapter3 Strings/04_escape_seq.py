f25ac461236c31e4964c30843c4a630c638367f5
Sushmita Malakar <sushmalakar10@gmail.com>
2025-12-11 09:39:51 +0545

    escape sequence

---
+++ Chapter3 Strings/04_escape_seq.py
@@ -0,0 +1,14 @@
+a = "Ram is a good boy\nbut not a bad boy" # new line
+print(a)
+
+b = "let\'s learn python"
+print(b)
+
+c = "this is a backslash :\\"
+print(c)
+
+d = "Ram is a good boy\nbut not a \"bad\" boy"
+print(d)
+
+e = "Let\'s go\t gym"
+print(e)
